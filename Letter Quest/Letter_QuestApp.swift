a33837dd841882162c3468322ed0236618a61b57
tanishksahni <tanishk.sahni2002@gmail.com>
2024-07-10 19:34:46 +0530

    Initial Commit

---
+++ Letter Quest/Letter_QuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Letter_QuestApp.swift
+//  Letter Quest
+//
+//  Created by Tanishk Sahni on 10/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Letter_QuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
